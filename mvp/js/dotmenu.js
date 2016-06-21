75bc8593f5b79939b0a486f15aed1f14ee60caeb
thibaultgouin <thibaultgouin@gmail.com>
2016-06-22 00:52:08 +0200

    debug du menu dot

--- mvp/js/dotmenu.js
+++ mvp/js/dotmenu.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         $("#first_home").css("background-color","transparent");
         $(".video1").fadeIn("200");
         $("#trois_home").css("top","100%");
+        $("#quatre_home").css("top","100%");
         $(this).addClass("active");
         $("#btn-2").removeClass("active");
         $("#btn-3").removeClass("active");
@@ -12,20 +13,22 @@ $(document).ready(function () {
     });
 
     $("#btn-2").click(function() {
-            $("#second_home").css({top: 0});
-            $(".video1").fadeOut("200");
-            $("#first_home").css("background-color","black");
-            $("#second_home").fadeIn("100");
-            $("#trois_home").css("top","100%");
-            $(this).addClass("active");
-            $("#btn-1").removeClass("active");
-            $("#btn-3").removeClass("active");
-            $("#btn-4").removeClass("active");
+        $("#second_home").css({top: 0});
+        $(".video1").fadeOut("200");
+        $("#first_home").css("background-color","black");
+        $("#second_home").fadeIn("100");
+        $("#trois_home").css("top","100%");
+        $("#quatre_home").css("top","100%");
+        $(this).addClass("active");
+        $("#btn-1").removeClass("active");
+        $("#btn-3").removeClass("active");
+        $("#btn-4").removeClass("active");
     });
 
     $("#btn-3").click(function() {
         $("#trois_home").css({top: 0});
         $("#trois_home").fadeIn("100");
+        $("#quatre_home").css("top","100%");
         $(this).addClass("active");
         $("#btn-1").removeClass("active");
         $("#btn-2").removeClass("active");
@@ -43,7 +46,10 @@ $(document).ready(function () {
     });
 
 
+
+
 });
+
 
 
 
