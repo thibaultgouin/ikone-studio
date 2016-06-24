a2e98a2b7042e722b94cf0f346a47c60eb66cf9e
thibaultgouin <thibaultgouin@gmail.com>
2016-06-24 15:17:49 +0200

    correction home page

--- mvp/js/script.js
+++ mvp/js/script.js
@@ -1,137 +1,142 @@
 $(document).ready(function () {
 
-
-    $('#first_home').on('mousewheel DOMMouseScroll MozMousePixelScroll', function (event, delta) {
-        if (delta < 0) {
-            $("#second_home").css({top: 0});
-            $(".video1").fadeOut("200");
-            $("#first_home").css("background-color", "black");
-            $("#second_home").fadeIn("100");
-            $("#btn-1").removeClass("active");
-            $("#btn-2").addClass("active");
-        }
-
-    });
-
-
-    $('#second_home').on('mousewheel DOMMouseScroll MozMousePixelScroll', function (event, delta) {
-        if (delta > 0) {
-            $("#second_home").css("top", "100%");
-            $("#first_home").css("background-color", "transparent");
-            $(".video1").fadeIn("200");
-            $("#btn-1").addClass("active");
-            $("#btn-2").removeClass("active");
-        }
-        if (delta < 0) {
-            $("#trois_home").css({top: 0});
-            /* $("#second_home").css("background-color","black");*/
-            $("#trois_home").fadeIn("100");
-            $("#btn-3").addClass("active");
-            $("#btn-2").removeClass("active");
-        }
-
-    });
-
-    $('#trois_home').on('mousewheel DOMMouseScroll MozMousePixelScroll', function (event, delta) {
-        if (delta > 0) {
-            $("#trois_home").css("top", "100%");
-            $("#second_home").css("top", "0");
-            $("#btn-2").addClass("active");
-            $("#btn-3").removeClass("active");
-        }
-
-        if (delta < 0) {
-            $("#quatre_home").css({top: 0});
-            $("#quatre_home").fadeIn("100");
-            $("#btn-4").addClass("active");
-            $("#btn-3").removeClass("active");
-        }
-
-    });
-
-    $('#quatre_home').on('mousewheel DOMMouseScroll MozMousePixelScroll', function (event, delta) {
-        if (delta > 0) {
-            $("#quatre_home").css("top", "100%");
-            $("#trois_home").css("top", "0");
-            $("#btn-4").removeClass("active");
-            $("#btn-3").addClass("active");
-        }
-
-    });
-
-
-    /*PAGE PROJET */
+    var index = 0;
 
     var $project = $(".list_projet").children();
     var indexMax = $project.length;
 
     var $bgproject = $(".bg_project").children();
     var projectMax = $bgproject.length;
-    var index = 0;
-    console.log(projectMax);
 
 
-    var nbrProject = 1;
+    var scrollTime = true;
+    var nbrProject = 0;
 
+
+    var $bghome = $(".list_home").children();
+    var homeMax = $bghome.length;
+    console.log($bghome.length + "nbr home");
+
+    var $menuDotMenu = $(".dotMenu ul").children();
+    var menuDotMax = $menuDotMenu.length;
+    console.log($menuDotMenu.length + "nbr menu dot");
+
+
+    /* ######## HOME PROJET ######## */
 
     $(document).on('mousewheel DOMMouseScroll MozMousePixelScroll', function (event, delta) {
+
+        /* $(".dotMenu li").click(function () {
+         nbrHome = $(this).attr("id").replace("btn-", "");
+         nbrHome = parseInt(nbrHome);
+         console.log(nbrHome + "numero");
+         }*/
+
         if (delta < 0) {
-            if (index == indexMax - 1) {
-                return;
-            }
-            $bgproject.eq(index).css("top", "100%");
-            $project.eq(index).removeClass("active");
-            index++;
-            $bgproject.eq(index).css("top", "0");
-            $(".title_project").css("top", 50 + (-index * 10) + "%");
-            $project.eq(index).addClass("active");
+            homeDown();
         }
 
+
         if (delta > 0) {
-            if (index == 0) {
-                return;
-            }
-            $bgproject.eq(index).css("top", "100%");
-            $project.eq(index).removeClass("active");
-            index--;
-            $bgproject.eq(index).css("top", "0");
-            $(".title_project").css("top", 50 - (index * 10) + "%");
-            $project.eq(index).addClass("active");
+            homeUp();
+
         }
 
     });
+
+
+    function homeDown() {
+        if (index == homeMax - 1 || scrollTime == false) {
+            return;
+        }
+        console.log(index + "nbr index");
+        scrollTime = false;
+        $menuDotMenu.eq(index).removeClass("active");
+        index++;
+        console.log(index + "nbr index");
+        $bghome.eq(index).css("top", "0");
+        $menuDotMenu.eq(index).addClass("active");
+        setTimeout(function () {
+            scrollTime = true;
+        }, 1100);
+
+    }
+
+    function homeUp() {
+        if (index == 0 || scrollTime == false) {
+            return;
+        }
+        scrollTime = false;
+        $bghome.eq(index).css("top", "100%");
+        $menuDotMenu.eq(index).removeClass("active");
+        index--;
+        $bghome.eq(index).css("top", "0");
+        $menuDotMenu.eq(index).addClass("active");
+        setTimeout(function () {
+            scrollTime = true;
+        }, 1100);
+
+    }
+
+
+
+    /****** FIN HOME PAGE ****/
 
     $(".list_projet li").click(function () {
         nbrProject = $(this).attr("id").replace("btn_project_", "");
         nbrProject = parseInt(nbrProject);
 
-
-
-        if(index < nbrProject ) {
-            if (index == indexMax - 1) {
-                return;
-            }
-            $bgproject.eq(index).css("top", "100%");
-            $project.eq(index).removeClass("active");
-            index++;
-            $bgproject.eq(index).css("top", "0");
-            $(".title_project").css("top", 50 + (-index * 10) + "%");
-            $project.eq(index).addClass("active");
+        if (index < nbrProject) {
+            projectDown();
         }
 
-        if(index > nbrProject ) {
-            if (index == 0) {
-                return;
-            }
-            $bgproject.eq(index).css("top", "100%");
-            $project.eq(index).removeClass("active");
-            index--;
-            $bgproject.eq(index).css("top", "0");
-            $(".title_project").css("top", 50 - (index * 10) + "%");
-            $project.eq(index).addClass("active");
+        if (index > nbrProject) {
+            projectUp();
         }
     });
 
+    function projectDown() {
+        if (index == indexMax - 1 || scrollTime == false) {
+            return;
+        }
+
+        scrollTime = false;
+        $bgproject.eq(index).css("top", "100%");
+        $project.eq(index).removeClass("active");
+        index++;
+        $bgproject.eq(index).css("top", "0");
+        $(".title_project").css("top", 50 + (-index * 10) + "%");
+        $project.eq(index).addClass("active");
+        setTimeout(function () {
+            scrollTime = true;
+        }, 800);
+        $(".design-2").css("top", "30%");
+    }
+
+
+
+
+
+    function projectUp() {
+        if (index == 0 || scrollTime == false) {
+            return;
+        }
+
+        scrollTime = false;
+        $bgproject.eq(index).css("top", "100%");
+        $project.eq(index).removeClass("active");
+        index--;
+        $bgproject.eq(index).css("top", "0");
+        $(".title_project").css("top", 50 - (index * 10) + "%");
+        $project.eq(index).addClass("active");
+        setTimeout(function () {
+            scrollTime = true;
+        }, 800);
+        $(".design-2").css("top", "10%");
+    }
+
+
+    /* ######## FIN PAGE PROJET ######## */
 
 
 });
