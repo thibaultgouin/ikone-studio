f746d0f57ff679be5f94174eb062b5d3b61e5d0d
thibaultgouin <thibaultgouin@gmail.com>
2016-06-30 21:47:35 +0200

    loading + nouvelle maquette

--- mvp/js/script.js
+++ mvp/js/script.js
@@ -24,97 +24,127 @@ $(document).ready(function () {
 
     /* ######## HOME PAGE ######## */
 
+    /* ####### LOADER######## */
+    $(window).load(function() {
+
+        setTimeout(function () {
+        $("#loading_home").css("margin-left","100%");
+        }, 3500);
+    });
 
 
-        $(document).on('mousewheel DOMMouseScroll MozMousePixelScroll', function (event, delta) {
+    $(document).on('mousewheel DOMMouseScroll MozMousePixelScroll', function (event, delta) {
 
-            /* $(".dotMenu li").click(function () {
-             nbrHome = $(this).attr("id").replace("btn-", "");
-             nbrHome = parseInt(nbrHome);
-             console.log(nbrHome + "numero");
-             }*/
+        /* $(".dotMenu li").click(function () {
+         nbrHome = $(this).attr("id").replace("btn-", "");
+         nbrHome = parseInt(nbrHome);
+         console.log(nbrHome + "numero");
+         }*/
 
-            if (delta < 0) {
-                homeDown();
-
-            }
-
-
-            if (delta > 0) {
-                homeUp();
-            }
-
-
-        });
-
-        $(".dotNav ul li").click(function () {
-            nbrHome = $(this).attr("id").replace("btn-", "");
-            nbrHome = parseInt(nbrHome);
-
-
-            if (index < nbrHome) {
-                scrollTime = false;
-                $menuDotMenu.eq(index).find('a').removeClass("active");
-                $menuDotMenu.eq(index - 1).find('a').removeClass("active");
-                $bghome.eq(nbrHome - 1).css("top", "0");
-                $menuDotMenu.eq(nbrHome - 1).find('a').addClass("active");
-                index = nbrHome - 1;
-                setTimeout(function () {
-                    scrollTime = true;
-                }, 1000);
-
-            }
-
-            if (index >= nbrHome) {
-                scrollTime = false;
-                $bghome.eq(index).css("top", "100%");
-                $menuDotMenu.eq(index).find('a').removeClass("active");
-                $menuDotMenu.eq(index - 1).find('a').removeClass("active");
-                $bghome.eq(nbrHome - 1).css("top", "0");
-                $menuDotMenu.eq(nbrHome - 1).find('a').addClass("active");
-                index = nbrHome - 1;
-                setTimeout(function () {
-                    scrollTime = true;
-                }, 1000);
-            }
-
-        });
-
-
-        function homeDown() {
-            if (index == homeMax - 1 || scrollTime == false) {
-                return;
-            }
-            scrollTime = false;
-            $menuDotMenu.eq(index).find('a').removeClass("active");
-            index++;
-            $bghome.eq(index).css("top", "0");
-            $menuDotMenu.eq(index).find('a').addClass("active");
-            setTimeout(function () {
-                scrollTime = true;
-            }, 1000);
-
-        }
-
-        function homeUp() {
-            if (index == 0 || scrollTime == false) {
-                return;
-            }
-            scrollTime = false;
-            $bghome.eq(index).css("top", "100%");
-            $menuDotMenu.eq(index).find('a').removeClass("active");
-            index--;
-            $bghome.eq(index).css("top", "0");
-            $menuDotMenu.eq(index).find('a').addClass("active");
-            setTimeout(function () {
-                scrollTime = true;
-            }, 1000);
+        if (delta < 0) {
+            homeDown();
 
         }
 
 
-
+        if (delta > 0) {
+            homeUp();
+        }
 
 
     });
+
+    $(".dotNav ul li").click(function () {
+        nbrHome = $(this).attr("id").replace("btn-", "");
+        nbrHome = parseInt(nbrHome);
+
+
+        if (index < nbrHome) {
+            scrollTime = false;
+            $menuDotMenu.eq(index).find('a').removeClass("active");
+            $menuDotMenu.eq(index - 1).find('a').removeClass("active");
+            $bghome.eq(nbrHome - 1).css("top", "0");
+            $menuDotMenu.eq(nbrHome - 1).find('a').addClass("active");
+            index = nbrHome - 1;
+            setTimeout(function () {
+                scrollTime = true;
+            }, 1000);
+
+        }
+
+        if (index >= nbrHome) {
+            scrollTime = false;
+            $bghome.eq(index).css("top", "100%");
+            $menuDotMenu.eq(index).find('a').removeClass("active");
+            $menuDotMenu.eq(index - 1).find('a').removeClass("active");
+            $bghome.eq(nbrHome - 1).css("top", "0");
+            $menuDotMenu.eq(nbrHome - 1).find('a').addClass("active");
+            index = nbrHome - 1;
+            setTimeout(function () {
+                scrollTime = true;
+            }, 1000);
+        }
+
+    });
+
+
+    function homeDown() {
+        if (index == homeMax - 1 || scrollTime == false) {
+            return;
+        }
+        scrollTime = false;
+        $menuDotMenu.eq(index).find('a').removeClass("active");
+        index++;
+        $bghome.eq(index).css("top", "0");
+        $menuDotMenu.eq(index).find('a').addClass("active");
+        setTimeout(function () {
+            scrollTime = true;
+        }, 1000);
+
+    }
+
+    function homeUp() {
+        if (index == 0 || scrollTime == false) {
+            return;
+        }
+        scrollTime = false;
+        $bghome.eq(index).css("top", "100%");
+        $menuDotMenu.eq(index).find('a').removeClass("active");
+        index--;
+        $bghome.eq(index).css("top", "0");
+        $menuDotMenu.eq(index).find('a').addClass("active");
+        setTimeout(function () {
+            scrollTime = true;
+        }, 1000);
+
+    }
+
+
+    $("#senti-1").click(function () {
+        $("#name_sentiment").text('LA JOIE');
+    });
+
+    $("#senti-2").click(function () {
+        $("#name_sentiment").text('PEUR');
+    });
+
+    $("#senti-3").click(function () {
+        $("#name_sentiment").text('TRISTESSE');
+    });
+
+    $("#senti-4").click(function () {
+        $("#name_sentiment").text('AMOUR');
+    });
+
+    $("#senti-5").click(function () {
+        $("#name_sentiment").text('COLERE');
+    });
+
+    $(".button_section_player").click(function () {
+        homeDown();
+
+    });
+
+
+});
 
