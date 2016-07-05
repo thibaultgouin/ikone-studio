be2d6789b46658f9eba292e994a06ca5954a0f19
alexb31 <alexandre@iMac-de-adminYkone-6.local>
2016-07-05 12:38:20 +0200

    Correction Video

--- mvp/js/video-fullscreen.js
+++ mvp/js/video-fullscreen.js
@@ -1,35 +1,51 @@
 $(function () {
 
-        $('body').on('webkitfullscreenchange mozfullscreenchange fullscreenchange', '.video-js', function (e) {
-            var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
-            var event = state ? 'FullscreenOn' : 'FullscreenOff';
-            if (event == 'FullscreenOn') {
-                console.log('play');
-                ($(this).find('video').first())[0].play();
-            } else {
-                ($(this).find('video').first())[0].pause();
-                console.log('pause');
-            }
-        });
+    var lastFullscreenPlayer;
 
-
-        function playVideoFullscreen(id) {
-            var video = document.getElementById(id);
-            //var video = document.querySelector('#'+ id +' video');
-            // go full-screen
-            if (video.requestFullscreen) {
-                video.requestFullscreen();
-            } else if (video.webkitRequestFullscreen) {
-                video.webkitRequestFullscreen();
-            } else if (video.mozRequestFullScreen) {
-                video.mozRequestFullScreen();
-            } else if (video.msRequestFullscreen) {
-                video.msRequestFullscreen();
-            }
+    $(document).on('mozfullscreenchange webkitfullscreenchange fullscreenchange', function (e) {
+        let element = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
+        if (!element) {
+            console.log('pause');
+            lastFullscreenPlayer.pause();
+        } else {
+            console.log('play');
+            lastFullscreenPlayer && lastFullscreenPlayer.pause();
+            lastFullscreenPlayer = ($(element).find('video').first())[0];
+            lastFullscreenPlayer.play();
         }
+    });
+
+
+    function playVideoFullscreen(id) {
+        var video = document.getElementById(id);
+        //var video = document.querySelector('#'+ id +' video');
+        // go full-screen
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen();
+        } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen();
+        } else if (video.msRequestFullscreen) {
+            video.msRequestFullscreen();
+        }
+    }
 
         $('#startVideoBtn').on('click', function () {
             playVideoFullscreen('vid1')
         });
+
+    ///// DEBUT ROMANTIQUE //////////
+
+
+    $('#play-dirty-dancing').on('click', function () {
+        playVideoFullscreen('dirty-dancing')
+    });
+
+    $('#play-notebook').on('click', function () {
+        playVideoFullscreen('notebook')
+    });
+
+    ///// FIN ROMANTIQUE //////////
 
     })
